bd15271955ed20ad9ad6cbdd73c8a4f20c590bfb
Joyce Carvalho <joycegcarvalho@hotmail.com>
2019-03-28 15:35:40 -0300

    Alterações aside

--- application/views/template-admin/aside.php
+++ application/views/template-admin/aside.php
@@ -1,49 +1,49 @@
 <!-- Sidebar menu-->>
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
-    <aside class="app-sidebar">
-      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image">
-        <div>
-          <p class="app-sidebar__user-name">John Doe</p>
-          <p class="app-sidebar__user-designation">Frontend Developer</p>
-        </div>
+  <aside class="app-sidebar">
+    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image">
+      <div>
+        <p class="app-sidebar__user-name">John Doe</p>
+        <p class="app-sidebar__user-designation">Frontend Developer</p>
       </div>
-      <ul class="app-menu">
-        <li><a class="app-menu__item <?=($menu_ativo == "dashboard") ? "active" : ""?>" href="<?=base_url("admin")?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Home</span></a></li>
-        <li class="treeview <?=($menu_ativo == "usuarios") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Usuários</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item <?=$submenu_ativo == "u_cad" ? "active" : ""?>" href="<?=base_url("user_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
-            <li><a class="treeview-item <?=$submenu_ativo == "u_list" ? "active" : ""?>" href="<?=base_url("user_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
-          </ul>
-        </li>
-        <li class="treeview <?=($menu_ativo == "facilitadores" ? "is-expanded" : "")?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-user"></i><span class="app-menu__label">Facilitador</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item <?=($submenu_ativo == "f_cad") ? "active" : ""?>" href="<?=base_url("facilitador_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
-            <li><a class="treeview-item <?=($submenu_ativo == "f_list") ? "active" : ""?>" href="<?=base_url("facilitador_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
-          </ul>
-        </li>
-        <li class="treeview <?=($menu_ativo == "parceiro") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-handshake-o "></i><span class="app-menu__label">Parceiros</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item <?=($submenu_ativo == "p_cad") ? "active" : "" ?>" href="<?=base_url("parceiros_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
-            <li><a class="treeview-item <?=($submenu_ativo == "p_list") ? "active" : "" ?>" href="<?=base_url("parceiros_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
-          </ul>
-        </li>
-        <li class="treeview <?=($menu_ativo == "curso") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Cursos</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item <?=(($submenu_ativo == "c_cad") ? "active" : "")?>" href="<?=base_url("curso_insert")?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
-            <li><a class="treeview-item <?=(($submenu_ativo == "c_list") ? "active" : "")?>" href="<?=base_url("curso_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
-          </ul>
-        </li>
-        <li class="treeview <?=($menu_ativo == "depoimentos") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-comments"></i><span class="app-menu__label">Depoimentos</span><i class="treeview-indicator fa fa-angle-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item <?=($submenu_ativo == "d_cad") ? "active" : "" ?>" href="<?=base_url("depoimentos_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
-            <li><a class="treeview-item <?=($submenu_ativo == "d_list") ? "active" : "" ?>" href="<?=base_url("depoimentos_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
-          </ul>
-        </li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-id-card-o"></i><span class="app-menu__label">Inscrições</span><i class="treeeview-indicator fa fa-angle-right"></i></a>
-          <ul class="treeview-menu">
-            <li><a class="treeview-item <?=(isset($submenu_ativo) and ($submenu_ativo == "i_cad") ? "active" : "")?>" href="<?=base_url()?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
-            <li><a class="treeview-item <?=(isset($submenu_ativo) and ($submenu_ativo == "i_list") ? "active" : "")?>" href="<?=base_url()?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
-          </ul>
-        </li>
-      </ul>
-    </aside>
+    </div>
+    <ul class="app-menu">
+      <li><a class="app-menu__item <?=($menu_ativo == "dashboard") ? "active" : ""?>" href="<?=base_url("admin")?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Home</span></a></li>
+      <li class="treeview <?=($menu_ativo == "usuarios") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i><span class="app-menu__label">Usuários</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <ul class="treeview-menu">
+          <li><a class="treeview-item <?=$submenu_ativo == "u_cad" ? "active" : ""?>" href="<?=base_url("user_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
+          <li><a class="treeview-item <?=$submenu_ativo == "u_list" ? "active" : ""?>" href="<?=base_url("user_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
+        </ul>
+      </li>
+      <li class="treeview <?=($menu_ativo == "facilitadores" ? "is-expanded" : "")?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-user"></i><span class="app-menu__label">Facilitador</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <ul class="treeview-menu">
+          <li><a class="treeview-item <?=($submenu_ativo == "f_cad") ? "active" : ""?>" href="<?=base_url("facilitador_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
+          <li><a class="treeview-item <?=($submenu_ativo == "f_list") ? "active" : ""?>" href="<?=base_url("facilitador_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
+        </ul>
+      </li>
+      <li class="treeview <?=($menu_ativo == "parceiro") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-handshake-o "></i><span class="app-menu__label">Parceiros</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <ul class="treeview-menu">
+          <li><a class="treeview-item <?=($submenu_ativo == "p_cad") ? "active" : "" ?>" href="<?=base_url("parceiros_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
+          <li><a class="treeview-item <?=($submenu_ativo == "p_list") ? "active" : "" ?>" href="<?=base_url("parceiros_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
+        </ul>
+      </li>
+      <li class="treeview <?=($menu_ativo == "curso") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Cursos</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <ul class="treeview-menu">
+          <li><a class="treeview-item <?=(($submenu_ativo == "c_cad") ? "active" : "")?>" href="<?=base_url("curso_insert")?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
+          <li><a class="treeview-item <?=(($submenu_ativo == "c_list") ? "active" : "")?>" href="<?=base_url("curso_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
+        </ul>
+      </li>
+      <li class="treeview <?=($menu_ativo == "depoimentos") ? "is-expanded" : "" ?>"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-comments"></i><span class="app-menu__label">Depoimentos</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+        <ul class="treeview-menu">
+          <li><a class="treeview-item <?=($submenu_ativo == "d_cad") ? "active" : "" ?>" href="<?=base_url("depoimentos_insert");?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
+          <li><a class="treeview-item <?=($submenu_ativo == "d_list") ? "active" : "" ?>" href="<?=base_url("depoimentos_list");?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
+        </ul>
+      </li>
+      <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-id-card-o"></i><span class="app-menu__label">Inscrições</span><i class="treeeview-indicator fa fa-angle-right"></i></a>
+        <ul class="treeview-menu">
+          <li><a class="treeview-item <?=($submenu_ativo == "i_cad") ? "active" : ""?>" href="<?=base_url()?>"><i class="icon fa fa-circle-o"></i> Cadastrar</a></li>
+          <li><a class="treeview-item <?=($submenu_ativo == "i_list") ? "active" : ""?>" href="<?=base_url()?>"><i class="icon fa fa-circle-o"></i> Listar</a></li>
+        </ul>
+      </li>
+    </ul>
+  </aside>
